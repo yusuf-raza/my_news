28ad140fe48a336d3de31f2903ff8cf6b42c4b48
Yusuf Raza <yousuf.raza@trax.pk>
2023-08-16 14:08:57 +0500

    added splash screen

--- lib/views/splash_screen_view.dart
+++ lib/views/splash_screen_view.dart
@@ -1,20 +1,67 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreenView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/views/home_screen_view.dart';
+
+class SplashScreenView extends StatefulWidget {
   const SplashScreenView({super.key});
 
+  @override
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
 
-
+class _SplashScreenViewState extends State<SplashScreenView> {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.sizeOf(context).height;
+    var width = MediaQuery.sizeOf(context).width;
 
-  var height = MediaQuery.sizeOf(context).height;
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.white
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        height: height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: SizedBox(
+              height: height * .5,
+              child: Image.asset(
+                "images/splash_icon.png",
+                fit: BoxFit.cover,
+              ),
+            )),
+            SizedBox(
+              width: width,
+              child: Center(
+                child: Text(
+                  "Top Headlines",
+                  style: GoogleFonts.aboreto(
+                      letterSpacing: .6, color: Colors.blue, fontSize: 24),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: width,
+              child: const SpinKitThreeInOut(
+                color: Colors.blue,
+                size: 25,
+              ),
+            ),
+          ],
+        ),
       ),
-      height: height,
-      child: Image.asset("images/splash_icon.png"),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const HomeScreenView()));
+    });
   }
 }
